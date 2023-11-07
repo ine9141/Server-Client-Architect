995ad9e53768fe7b62ec39c15e7db1090b676150
LSM <dltmdals6718@naver.com>
2023-11-07 16:43:53 +0900

    라운드와 시간으로 string 메시지 반환

--- src/server/core/handler/MatrixHandler.java
+++ src/server/core/handler/MatrixHandler.java
@@ -2,11 +2,38 @@ package server.core.handler;
 
 public class MatrixHandler {
 
-    private static int[][][] maxtrix;
+    private static int[][][] maxtrix = new int[6][10][10];
 
     // 행렬 생성
     public static void createMatrix() {
         maxtrix = new int[6][10][10];
+    }
+
+    // 현재 모든 행렬 및 시간 찍기
+    public static String printMatrix(int round, int time) {
+
+        // [라운드 : 1, 소요시간 : 1]
+        // [1번째 행렬]
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0
+        // [2번째 행렬]
+        // 1 1 1
+        // 1 1 1
+        // 1 1 1
+
+        String string = "[라운드 : " + round + ", 소요시간 : " + time + "]\n";
+        for(int i=0; i<6; i++) {
+            string = string.concat("[" + (i+1) + "]번째 행렬\n");
+            for(int j=0; j<10; j++) {
+                for(int k=0; k<10; k++) {
+                    string =string.concat(String.valueOf(maxtrix[i][j][k])+" ");
+                }
+                string =string.concat("\n");
+            }
+        }
+        string = string.concat("\n");
+        return string;
     }
 
     public static int[][][] getMaxtrix() {
