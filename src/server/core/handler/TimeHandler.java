e18940b1524361301425789026cc113ebe82c5b7
yeon228 <cumber88@naver.com>
2023-11-07 11:37:08 +0900

    serverTime, clientTime, totalTime 분리

--- src/server/core/handler/TimeHandler.java
+++ src/server/core/handler/TimeHandler.java
@@ -1,17 +1,35 @@
 package server.core.handler;
 
 public class TimeHandler {
-    private static int time;
+    private static int[] clientTime;//클라이언트별 시간
+    private static int serverTime;
     private static int round;
+    private static int totalTime;
 
     public TimeHandler(){
-        time = 0; round = 0;
+        clientTime = new int[4]; round = 0; totalTime = 0; serverTime = 0;
+        for (int i = 0; i < 4; i++){
+            clientTime[i] = 0;
+        }
     }
+
+    //Server 동작 때 마다 이 메서드 call
     public static void addTime(){
-        time++;
+        serverTime++;
+        totalTime++;
+    }
+    //Client 동작 때 마다 이 메서드 call
+    public static void addTime(String clientInfo){
+        clientTime[Integer.parseInt(clientInfo)]++;
+        totalTime++;
     }
 
     public static void addRound(){
+        for (int i = 0; i < 4; i++){
+            totalTime += clientTime[i];
+            clientTime[i] = 0;
+        }
+        serverTime = 0;
         round++;
     }
 
@@ -19,12 +37,20 @@ public class TimeHandler {
         return round;
     }
 
-    public static int getTime(){
-        return time;
+    public static int getTotalTime(){
+        return totalTime;
+    }
+
+    public static int getClientTime(String clientInfo){
+        return clientTime[Integer.parseInt(clientInfo)];
+    }
+
+    public static int getServerTime(){
+        return serverTime;
     }
 
     @Override
     public String toString(){
-        return "Round : " + round + ", Time : " + time;
+        return "Round : " + round + ", Total : " + totalTime;
     }
 }
