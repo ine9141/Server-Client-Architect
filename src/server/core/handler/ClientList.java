5d28598bd647cc9d178f0a0421ada41fbf79cd00
LSM <dltmdals6718@naver.com>
2023-11-10 15:07:17 +0900

    순서쌍 번호를 주면 역할을 배정

--- src/server/core/handler/ClientList.java
+++ src/server/core/handler/ClientList.java
@@ -37,7 +37,7 @@ public class ClientList {
     // 순서쌍의 번호로 해당 순서쌍의 Socket를 획득합니다.
     public static Socket[] getSocketPair(int num) {
         Socket[] sockets = new Socket[2];
-
+        // "row_mat", "col_mat", "calc1", "calc2"
         // 0  1  2  3  4  5
         // AB AC AD BC BD CD
         if(num==0) {
@@ -60,6 +60,41 @@ public class ClientList {
             sockets[1] = clients[3];
         }
         return sockets;
+    }
+
+    // 역할 배정 (현재 순서쌍 번호를 주면 역할을 설정해줌)
+    public static void setRole2(int num) {
+        if(num==0) {
+            Role[0] = "row_mat";
+            Role[1] = "col_mat";
+            Role[2] = "calc1";
+            Role[3] = "calc2";
+        } else if(num==1) {
+            Role[0] = "row_mat";
+            Role[1] = "calc1";
+            Role[2] = "col_mat";
+            Role[3] = "calc2";
+        } else if(num==2) {
+            Role[0] = "row_mat";
+            Role[1] = "calc1";
+            Role[2] = "calc2";
+            Role[3] = "col_mat";
+        } else if(num==3) {
+            Role[0] = "calc1";
+            Role[1] = "row_mat";
+            Role[2] = "col_mat";
+            Role[3] = "calc2";
+        } else if(num==4) {
+            Role[0] = "calc1";
+            Role[1] = "row_mat";
+            Role[2] = "calc2";
+            Role[3] = "col_mat";
+        } else if(num==5) {
+            Role[0] = "calc1";
+            Role[1] = "calc2";
+            Role[2] = "row_mat";
+            Role[3] = "col_mat";
+        }
     }
 
     // 역할 랜덤 배정
