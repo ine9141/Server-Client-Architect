f914bcf43ce0d3f458ebc51b0a277de28c4848d5
yeon228 <cumber88@naver.com>
2023-11-10 14:43:17 +0900

    내부클래스 제거

--- src/server/core/handler/ClientHandler.java
+++ src/server/core/handler/ClientHandler.java
@@ -1,2 +1,70 @@
-package server.core.handler;public class ClientHandler {
+package server.core.handler;
+
+import server.core.Instruction;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+
+    private Socket socket;
+
+    private ServerSocket serverSocket;
+
+    public ClientHandler(Socket socket, ServerSocket serverSocket) {
+        this.socket = socket;
+        this.serverSocket = serverSocket;
+    }
+
+    @Override
+    public void run() {
+        String clientIP = null;
+        try {
+            int[][] matrix = new int[10][10];
+
+            clientIP = socket.getRemoteSocketAddress().toString();
+            ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream());
+
+            while(true) {
+                objectOutput.writeObject(new Instruction(true,false,false)); //이게 클라이언트한테 명령하는 느낌
+
+                //차례대로 행 입력
+                objectOutput.writeObject(new Instruction(false,true,true));
+                int line1 = (int) objectInput.readObject();
+                int[] matrix1 = (int[]) objectInput.readObject();
+
+                //열 입력
+                objectOutput.writeObject(new Instruction(false, true, false));
+                int line2 = (int) objectInput.readObject();
+                int[] matrix2 = (int[]) objectInput.readObject();
+
+                objectOutput.writeObject(new Instruction(false,false,false));
+                objectOutput.writeObject(matrix1);
+                objectOutput.writeObject(matrix2);
+
+                int answer = (int) objectInput.readObject();
+
+                matrix[line1][line2] = answer;
+
+                System.out.println(matrix[line1][line2]);
+                break;
+            }
+
+        } catch (IOException e) {
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                serverSocket.close();
+                socket.close(); // 해당 클라이언트와의 소켓 닫기
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
